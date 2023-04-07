f7e3ff5c801410ca7192d281c53d0a35347d537d
Suryanto <suryanto.c@weefer.co.id>
2023-04-07 21:16:14 +0700

    Changed 404 page not found layout and styling

--- src/screens/PageNotFound/index.js
+++ src/screens/PageNotFound/index.js
@@ -1,34 +1,47 @@
 import React from "react";
-import { Row, Col } from "react-bootstrap";
-import Illustration from "../../assets/404.jpg";
+import { Container, Row, Col } from "react-bootstrap";
+import Illustration from "../../assets/404.svg";
 
 function PageNotFound() {
   return (
-    <Row className="mx-0 cst-pageNotFound-row-h">
-      <Col
-        md={12}
-        lg={6}
-        className="d-flex justify-content-center align-items-center"
-      >
-        <div className="cst-pageNotFound-img-frame">
-          <img alt="" src={Illustration} className="cst-pageNotFound-img" />
-        </div>
-      </Col>
-      <Col md={12} lg={6} className="d-flex px-md-0">
-        <div className="text-center p-4 mx-auto ms-md-5 my-auto">
-          <h1 className="cst-text-primary">
-            <strong>404</strong>
-          </h1>
-          <h2>
-            <strong>Halaman Tidak Ditemukan</strong>
-          </h2>
-          <p>Kami tidak dapat menemukan halaman yang anda cari</p>
-          <p className="cst-text-secondary">
-            Apakah anda yakin sudah mengakses URL yang benar?
-          </p>
-        </div>
-      </Col>
-    </Row>
+    <Container
+      fluid
+      className="cst-min-h-md d-flex flex-column justify-content-center"
+    >
+      <Row className="mx-0 d-flex justify-content-center">
+        <Col xs={8} sm={5} md={5} lg={5} xl={3}>
+          <img
+            style={{ width: "95%", height: "95%" }}
+            alt=""
+            src={Illustration}
+          />
+        </Col>
+        <Col xs={12} sm={12} md={12} lg={12} xl={5} className="d-flex">
+          <div className="pt-3 px-4 m-auto my-lg-auto mx-xl-0 text-center text-xl-start">
+            <h1 className="cst-text-primary">
+              <strong>Opss...</strong>
+            </h1>
+            <h2>
+              <strong>Halaman Tidak Ditemukan</strong>
+            </h2>
+            <p>
+              Halaman yang anda cari
+              <span className="cst-text-primary fw-bold"> belum tersedia </span>
+              atau
+              <span className="cst-text-primary fw-bold">
+                {" "}
+                telah dipindahkan{" "}
+              </span>
+            </p>
+            <p className="cst-text-secondary my-0 my-xl-5">
+              Apakah anda yakin sudah mengakses
+              <span className="cst-text-primary fw-bold"> URL </span>
+              yang benar?
+            </p>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
